d9f32f9cbca913fe3902e616ad79872e207f9c41
Geetha Arul <geetha.arul@tagitmobile.com>
2020-12-27 18:20:04 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDf8_u8vGMwDVTs6Sbmez96t0-J83QdHE8",
+    authDomain: "ng-fitness-tracker-6c80b.firebaseapp.com",
+    projectId: "ng-fitness-tracker-6c80b",
+    storageBucket: "ng-fitness-tracker-6c80b.appspot.com",
+    messagingSenderId: "365110576375",
+    appId: "1:365110576375:web:4bc7ce3442da12cd1ebff2",
+    measurementId: "G-KEFRKLCDX3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDf8_u8vGMwDVTs6Sbmez96t0-J83QdHE8",
+    authDomain: "ng-fitness-tracker-6c80b.firebaseapp.com",
+    projectId: "ng-fitness-tracker-6c80b",
+    storageBucket: "ng-fitness-tracker-6c80b.appspot.com",
+    messagingSenderId: "365110576375",
+    appId: "1:365110576375:web:4bc7ce3442da12cd1ebff2",
+    measurementId: "G-KEFRKLCDX3"
+  }
 };
 
 /*
